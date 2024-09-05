9c0cdb28cc4fcf9aefff3a78ddb164c2a89ce3df
ramarajan007 <ramarajan581995@gmail.com>
2024-09-05 14:21:47 +0530

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -7,14 +7,14 @@ $i = 0;
 /* Authentication type */
 /* Server parameters */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
-$cfg['Servers'][$i]['verbose'] = 'local-mysql';
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['verbose'] = 'Server-1';
+$cfg['Servers'][$i]['host'] = '192.168.120.85';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 $i++;
 
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
-$cfg['Servers'][$i]['verbose'] = 'docker-mysql';
+$cfg['Servers'][$i]['verbose'] = 'Server-2';
 $cfg['Servers'][$i]['host'] = '172.17.0.3';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
