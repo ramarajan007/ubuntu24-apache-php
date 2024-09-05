3a1590b142c85c85292845b7377337536001b79e
ramarajan007 <ramarajan581995@gmail.com>
2024-09-05 14:19:35 +0530

    Update config-db.php

--- config-db.php
+++ config-db.php
@@ -1,18 +1,24 @@
-<?php
-##
-## database access settings in php format
-## automatically generated from /etc/dbconfig-common/phpmyadmin.conf
-## by /usr/sbin/dbconfig-generate-include
-##
-## by default this file is managed via ucf, so you shouldn't have to
-## worry about manual changes being silently discarded.  *however*,
-## you'll probably also want to edit the configuration file mentioned
-## above too.
-##
-$dbuser='';
-$dbpass='';
-$basepath='';
-$dbname='';
-$dbserver='127.0.0.1';
-$dbport='3306';
-$dbtype='mysql';
+// Analyse the Existing config.inc.php file and add the below lines in it. To Access the multiple MySQL host from Single PHPMyadmin Files.
+$i = 0;
+
+/**
+ * First server
+ */
+/* Authentication type */
+/* Server parameters */
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+$cfg['Servers'][$i]['verbose'] = 'local-mysql';
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$i++;
+
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+$cfg['Servers'][$i]['verbose'] = 'docker-mysql';
+$cfg['Servers'][$i]['host'] = '172.17.0.3';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$i++;
+
+/* Server parameters */
+
